781d13460cda59df72eff3371cee84c9c89ba330
Greg Kapp <gregkapp@gmail.com>
2013-04-02 16:32:55 -0700

    removed old file

--- Chapters 1-7/name_if.rb
+++
@@ -1,9 +0,0 @@
-puts "What is your name?"
-print "> "
-name = gets.chomp
-
-if (name.capitalize == "Greg") or (name.capitalize == "Lisa")
-  puts "Good name!"
-else
-  puts "I guess you got an alright name..."
-end
