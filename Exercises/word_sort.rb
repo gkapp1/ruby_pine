913f5bd0896eea2e77f5dbc1969d50bfcea81768
Greg Kapp <gregkapp@gmail.com>
2013-04-02 22:49:00 -0700

    WORD SORT

---
+++ Exercises/word_sort.rb
@@ -0,0 +1,38 @@
+require 'open-uri'
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = [] 
+
+open(WORD_URL) {|f| 
+  f.each_line {|word| WORDS.push(word.downcase.chomp)}
+}
+
+def sorter(array)
+  
+  sorted_words = []
+  
+  while array != []
+    n = array.length - 1
+    i = 0
+    current_winner = array[i]
+    current_position = i
+    while i + 1 <= n
+      if current_winner < array[i+1]
+        current_winner = current_winner
+        current_position = current_position
+      else
+        current_winner = array[i+1]
+        current_position = i + 1
+      end
+      i = i + 1
+    end
+    sorted_words.push(current_winner)
+    array.delete_at(current_position)
+  end
+  puts sorted_words
+end
+  
+puts sorter(WORDS)
+
+hell = ["zebra","cat","dog","apple", "cat", "banana","charlie","finger","animal","cat", "puppy"]
+puts sorter(hell)
---
+++ README.md
@@ -0,0 +1,4 @@
+Just some random exercises taken from "Learn to Program: Second Edition" by Chris Pine.
+
+Author: Greg Kapp
+Location: San Francisco, CA
