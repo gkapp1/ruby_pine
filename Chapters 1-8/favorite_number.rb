bd29f6ac192d542e086de967e8db9e768acc2dd7
Greg Kapp <gregkapp@gmail.com>
2013-04-02 20:47:06 -0700

    removed old folder

--- Chapters 1-8/favorite_number.rb
+++
@@ -1,9 +0,0 @@
-puts "Hey man, what's your favorite number?"
-print "> "
-favorite_number = gets.chomp
-
-puts "Cool but check-it: "
-favorite_number = favorite_number.to_i + 1
-puts "How about your new favorite number be " + favorite_number.to_s + "?"
-#puts "How about your new favorite number be #{favorite_number}?" #Sweet Alternative...
-puts "That shit is BIGGER, BETTER, AND BADDER -- KNOW WHAT I'M SAYING?"
--- Chapters 1-8/mean_boss.rb
+++
@@ -1,7 +0,0 @@
-puts 'What do YOU want?'
-print "> "
-request = gets.chomp
-request_upcase = request.upcase
-
-puts 'WHADDAYA MEAN "' + request_upcase + '"?!? YOU\'RE FIRED!!'
-#puts "WHADDAYA MEAN \"#{request_upcase}\"?!? YOU\'RE FIRED!!" # More elegant
