1183e390d57c7d7e69402129d372145f7b9973a8
Greg Kapp <gregkapp@gmail.com>
2013-04-03 17:17:00 -0700

    Improved Word Sorting Program

--- Exercises/dictionary_sort.rb
+++ Exercises/dictionary_sort.rb
@@ -1,40 +1,50 @@
 require 'open-uri'
 
 WORD_URL = "http://learncodethehardway.org/words.txt"
-WORDS = [] 
+WORDS = []
 
 open(WORD_URL) {|f| 
   f.each_line {|word| WORDS.push(word.downcase.chomp)}
 }
 
-def sorter(array)
+def dictionary_sorter(array)
+  
+  downcase_array = []
+  
+  j =0
+  x = array.length
+  while j < x
+    downcase_array[j] = array[j].downcase
+    j = j + 1
+  end
   
   sorted_words = []
   
-  while array != []
-    n = array.length - 1
+  while downcase_array != []
+    n = downcase_array.length - 1
     i = 0
-    current_winner = array[i]
-    current_position = i
+    current_position = 0
+    current_winner = downcase_array[i]
     while i < n
-      if current_winner < array[i+1]
+      if current_winner < downcase_array[i+1]
         current_winner = current_winner
         current_position = current_position
       else
-        current_winner = array[i+1]
+        current_winner = downcase_array[i+1]
         current_position = i + 1
       end
       i = i + 1
     end
-    sorted_words.push(current_winner)
+    sorted_words.push(array[current_position])
+    downcase_array.delete_at(current_position)
     array.delete_at(current_position)
   end
   puts sorted_words
 end
   
-sorter(WORDS)
+dictionary_sorter(WORDS)
 
 puts "-------" * 5
 
 random_words = ["Zebra","cat","dog","apple", "cat", "banana","charlie","finger","animal","cat", "puppy"]
-sorter(random_words)
+dictionary_sorter(random_words)
