fc5fa65fb296b1b9695c8a306cc0fcac715c8ce1
Greg Kapp <gregkapp@gmail.com>
2013-06-01 14:46:05 -0700

    New dragon program that permits user input via Terminal to 'play'

---
+++ Exercises/dragon_user.rb
@@ -0,0 +1,137 @@
+class Dragon
+  
+  def initialize name
+    @name = name
+    @asleep = false
+    @stuff_in_belly = 10    # He is full.
+    @stuff_in_intestine = 0 # He doesn't need to go.
+    
+    puts "#{@name} is born!"
+  end
+  
+  def feed
+    puts "You feed #{@name}."
+    @stuff_in_belly = 10
+    passage_of_time
+  end
+  
+  def walk
+    puts "You walk #{@name}."
+    @stuff_in_intestine = 0 # He goes potty...
+    passage_of_time
+  end
+  
+  def put_to_bed
+    puts "You put #{@name} to bed."
+    @asleep = true
+    3.times do
+      if @asleep
+        passage_of_time
+      end
+      if @asleep
+        puts "#{@name} snores, filling the room with smoke."
+      end
+    end
+    if  @asleep 
+      @asleep = false
+      puts "#{@name} wakes up slowly."
+    end
+  end
+  
+  def toss
+    puts "You toss #{@name} up into the air."
+    puts "He giggles, which singes your eyebrows."
+    passage_of_time
+  end
+  
+  def rock
+    puts "You rock #{@name} gently."
+    @asleep = true
+    puts "He briefly dozes off..."
+    passage_of_time
+    if @asleep
+      @aslep = false
+      puts "...but wakes when you stop."
+    end
+  end
+  
+  private
+  
+  def hungry?
+    @stuff_in_belly <= 2
+  end
+  
+  def poopy?
+    @stuff_in_intestine >= 8
+  end
+  
+  def passage_of_time
+    if @stuff_in_belly > 0
+      # Move food from belly to intestine
+      @stuff_in_belly = @stuff_in_belly - 1
+      @stuff_in_intestine = @stuff_in_intestine + 1
+    else # Our dragon is starving!
+      if @asleep
+        @asleep = false
+        puts "He wakes up suddenly!"
+      end
+      puts "#{@name} is starving! In desperation, he ate YOU!"
+      exit
+    end
+    
+    if @stuff_in_intestine >= 10
+      @stuff_in_intestine = 0
+      puts "Whoops! #{@name} had an accident..."
+    end
+    
+    if hungry?
+      if @asleep
+        @asleep = false
+        puts "He wakes up suddenly!"
+      end
+      puts "#{@name}'s stomach grumbles..."
+    end
+    
+    if poopy?
+      if @asleep
+        @asleep = false
+        puts "He wakes up suddenly!"
+      end
+      puts "#{@name} does the potty dance..."
+    end
+  end        
+  
+end
+
+# USER INPUT  
+
+puts "What do you want to name your dragon?"
+print "> "
+name = gets.chomp
+
+pet = Dragon.new name
+
+while true
+
+  puts "Now what do you want to do with your pet dragon?"
+  puts "Here are your choices (case-sensitive): feed, walk, put_to_bed, toss, rock, exit."
+  print "> "
+
+  choice = gets.chomp
+
+  if choice == "feed"
+    pet.feed
+  elsif choice == "walk"
+    pet.walk
+  elsif choice == "put_to_bed"
+    pet.put_to_bed
+  elsif choice == "toss"
+    pet.toss
+  elsif choice == "rock"
+    pet.rock
+  elsif choice == "exit"
+    exit
+  else
+    puts "Sorry - didn't get that. Please enter your choice again."
+  end
+end
